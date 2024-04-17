631764b33674ae98a811bdc0b4f329c19a4a4557
Andriy Shutka <shutkaintel98@gmail.com>
2024-04-17 08:19:20 +0300

    tiHaveText

--- test/searchDropdownElements.spec.js
+++ test/searchDropdownElements.spec.js
@@ -21,7 +21,8 @@ await page.goto('https://openweathermap.org/')
 const searchField =  page.getByPlaceholder('Search city')
 searchField.fill('Washington')
 await page.locator('button[type="submit"]').click()
-await expect(page.locator('ul.search-dropdown-menu')).toBeVisible()
+await expect(page.locator('ul.search-dropdown-menu')).toHaveText(['Washington, US ', 'Washington, D.C., US ','Washington County, US ', 'Washington County, US '])
+await page.pause(2000);
 await page.getByText('Washington, US ').click()
 await expect(page.getByRole('heading', {name:'Washington, US'})).toBeVisible()
 
