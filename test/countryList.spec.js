2ae8209e063c0257694c03a5e55d8babae18103b
Andriy Shutka <shutkaintel98@gmail.com>
2024-04-21 12:26:35 +0300

    before

--- test/countryList.spec.js
+++ test/countryList.spec.js
@@ -1,1 +1,10 @@
 import {test, expect} from '@playwright/test';
+ 
+ test.describe('Search field functionality', () =>{
+    test.beforeEach('Open the main page', async({page})=>{
+test('Search box  availability on all pages'),
+
+
+
+    })
+})
