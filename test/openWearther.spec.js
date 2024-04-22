965cb4eaa50380b08eb25e60ac7d2359d906b3bb
Andriy Shutka <shutkaintel98@gmail.com>
2024-04-22 13:38:58 +0300

    new

--- test/openWearther.spec.js
+++ test/openWearther.spec.js
@@ -18,22 +18,3 @@
 
 // })
 // })
-import {test, expect} from '@playwright/test';
- 
-test.describe('Search field functionality', () =>{
-    test.beforeEach('Open the main page', async({page})=>{
-         await page.goto('https://magento.softwaretestingboard.com/')
-
-test('Navigation after selecting Item from drop-down list with valid input'), async({page})=>{
-    const searchField = await page.locator('#search')
-    searchField.fill('shorts')
-    await expect(page.waitForSelector('#search_autocomplete')).toBeVisible()
-    await page.locator('#qs-option-0').click()
-    const urlItm_1 = 'https://magento.softwaretestingboard.com/catalogsearch/result/?q=shorts'
-    expect(page.url()).toEqual(urlItm_1)
-    }
-
-
-
-    })
-})
--- test/searchField.spec.js
+++ test/searchField.spec.js
@@ -1,19 +1,32 @@
-import {test, expect} from '@playwright/test';
+// import {test, expect} from '@playwright/test';
  
-test.describe('Search field functionality', () =>{
-    test.beforeEach('Open the main page', async({page})=>{
-         await page.goto('https://magento.softwaretestingboard.com/')
+// test.describe('Search field functionality', () =>{
+//     test.beforeEach('Open the main page', async({page})=>{
+//          await page.goto('https://magento.softwaretestingboard.com/')
 
-test('Navigation after selecting Item from drop-down list with valid input'), async({page})=>{
-    const searchField = await page.locator('#search')
-    searchField.fill('shorts')
-    await expect(page.waitForSelector('#search_autocomplete')).toBeVisible()
-    await page.locator('#qs-option-0').click()
-    const urlItm_1 = 'https://magento.softwaretestingboard.com/catalogsearch/result/?q=shorts'
-    expect(page.url()).toEqual(urlItm_1)
-    }
+// test('Navigation after selecting Item from drop-down list with valid input'), async({page})=>{
+//     const searchField = await page.locator('#search')
+//     searchField.fill('shorts')
+//     await expect(page.waitForSelector('#search_autocomplete')).toBeVisible()
+//     await page.locator('#qs-option-0').click()
+//     const urlItm_1 = 'https://magento.softwaretestingboard.com/catalogsearch/result/?q=shorts'
+//     expect(page.url()).toEqual(urlItm_1)
+//     }
 
 
 
-    })
-})
+//     })
+// })
+import { test, expect } from '@playwright/test';
+
+test.describe('Search field functionality', () => {
+    test('Navigation after selecting Item from drop-down list with valid input', async ({ page }) => {
+        await page.goto('https://magento.softwaretestingboard.com/');
+        const searchField = await page.locator('#search');
+        await searchField.fill('shorts');
+        await expect(page.waitForSelector('#search_autocomplete')).toBeVisible();
+        await page.locator('#qs-option-0').click();
+        const urlItm_1 = 'https://magento.softwaretestingboard.com/catalogsearch/result/?q=shorts';
+        expect(page.url()).toEqual(urlItm_1);
+    });
+});
